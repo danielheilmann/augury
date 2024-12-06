6c1adac3f0221ff7721e0eb5c4646ac7cc560f9d
Noxden <noxden@hotmail.com>
2024-12-06 01:03:50 +0100

    Corrected spelling

--- BachelorProject/Assets/Scripts/Settings.cs
+++ BachelorProject/Assets/Scripts/Settings.cs
@@ -2,15 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Settings : MonoBehaviour
+public static class Settings
 {
     public static bool OpenExplorerOnSave = false;
-    public static float expectedSessionRuntimeInMinutes = 1f;  //< This value is used to calculate the initial buffer size for the gazepoint dictionary based on session duration and tick rate.
+    public static float ExpectedSessionRuntimeInMinutes = 1f;  //< This value is used to calculate the initial buffer size for the gazepoint dictionary based on session duration and tick rate.
     public static int PrettyJSONExportIndent = 1;   //< 0 = off; 1 = on; >1 = increased indentation  //* Turning it off disables pretty formatting, which will reduce file size and is therefore recommended.
-    private void Start()
-    {
-        // Set up Frame Limiter
-        // Application.targetFrameRate = 60;
-        // QualitySettings.vSyncCount = 1;
-    }
+    // private void Start()
+    // {
+    //     // Set up Frame Limiter
+    //     // Application.targetFrameRate = 60;
+    //     // QualitySettings.vSyncCount = 1;
+    // }
 }
