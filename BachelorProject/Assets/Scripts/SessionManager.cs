06a53269909a2683302df00c46471a08b4d46cc9
Noxden <noxden@hotmail.com>
2025-01-01 15:44:32 +0100

    Updated SessionManager to handle distinction between Record and Replay
    ### Description
    - Now the SessionManager contains UnityEvents for the Start and Stop of the Record and Replay mode respectively. This will be the main driver for the Recording and Replaying functionalities of other scripts, which currently are all simply executed on Start() with no distinction between the modes.
    
    ### Further Changes
    - Turned the time format string into a const variable to improve documentation clarity

--- BachelorProject/Assets/Scripts/SessionManager.cs
+++ BachelorProject/Assets/Scripts/SessionManager.cs
@@ -1,24 +1,67 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class SessionManager : MonoBehaviour
 {
-    public static DateTime sessionStartTime;
-    /// <summary>
-    /// Ensures system file name compliancy by using the custom format "yyyy-MM-dd HH-mm-ss".
-    /// </summary>
-    public static string sessionIdentifier;
+    private enum DataMode { Record, Replay }
 
-    private void Start() //TODO: Should trigger OnRecordingStart instead
+    private const string FileNameSafeTimeFormat = "yyyy-MM-dd HH-mm-ss"; //< Ensures system file name compliance by using dashes as delimiter characters.
+
+    public static SessionManager Instance { get; private set; } //< Is important to ensure the sessionStartTime is not overwritten after e.g. a scene change (into a scene that also contains a SessionManager).
+    public static UnityEvent OnRecordStart { get; private set; } = new();
+    public static UnityEvent OnRecordStop { get; private set; } = new();
+    public static UnityEvent OnReplayStart { get; private set; } = new();
+    public static UnityEvent OnReplayStop { get; private set; } = new();
+    public static DateTime sessionStartTime { get; private set; }
+    public static string sessionIdentifier { get; private set; }
+
+    [SerializeField] private DataMode mode = DataMode.Record;
+
+    private void Awake()
     {
-        StartSession();
+        if (Instance == null)
+            Instance = this;
+        else
+        {
+            Debug.LogError($"{this} cannot set itself as instance as one has already been set by {Instance.gameObject}. Deleting self.");
+            Destroy(this);
+        }
     }
 
-    public static void StartSession()
+    private void Start()
     {
-        sessionStartTime = DateTime.Now;
-        sessionIdentifier = $"Session {sessionStartTime.ToString("yyyy-MM-dd HH-mm-ss", System.Globalization.CultureInfo.InvariantCulture)}"; //! Pay attention that this is format is not the same as the FileSystemHandler.TimestampFormat, as that one is not "file name compliant"! (will lead to error due to the colons)
+        switch (mode)
+        {
+            case DataMode.Record:
+                StartRecording();
+                break;
+            case DataMode.Replay:
+                StartReplaying();
+                break;
+        }
+    }
+
+    private void StartRecording()
+    {
+        Debug.Log($"Started Record Mode.");
+
+        sessionStartTime = Timer.latestTimestamp;
+        sessionIdentifier = $"Session {sessionStartTime.ToString(FileNameSafeTimeFormat, System.Globalization.CultureInfo.InvariantCulture)}"; //! Pay attention that this is format is not the same as the FileSystemHandler.TimestampFormat, as that one is not "file name compliant"! (will lead to error due to the colons)
+
+        OnRecordStart.Invoke();
+    }
+
+    private void StartReplaying()
+    {
+        Debug.Log($"Started Replay Mode.");
+
+        // Execute any other code.
+
+        OnReplayStart.Invoke();
     }
 }
