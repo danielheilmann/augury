57d9e3f36b4acdc0128cbaf064ae752bf531d731
Noxden <noxden@hotmail.com>
2025-01-21 23:02:22 +0100

    feat: Turned ReplayAction into abstract class
    to prevent direct instantiation

--- BachelorProject/Assets/Augury/Scripts/ReplayAction.cs
+++ BachelorProject/Assets/Augury/Scripts/ReplayAction.cs
@@ -2,9 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-using SimpleJSON;
 
-public class ReplayAction
+public abstract class ReplayAction
 {
     public enum ActionType { GazePoint, DynamicObject }
 
